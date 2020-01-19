3b49b20a8811badcff180f4b389ea39e9cdf919c
Nino Floris <mail@ninofloris.com>
2020-01-19 20:49:15 +0100

    Fix conformance tests, change package targets to copy to output instead of publish and bump version

--- test/Crowded.MediaTypes.PackageTest/ConformanceTests.cs
+++ test/Crowded.MediaTypes.PackageTest/ConformanceTests.cs
@@ -12,7 +12,7 @@ namespace Crowded.MediaTypes.PackageTest
         {
             Assert.Throws<ArgumentException>(() => new Magic(Magic.DefaultMagicFlags, null));
         }
-        
+
         [Fact]
         public void LoadLibSucceeds()
         {
@@ -26,40 +26,46 @@ namespace Crowded.MediaTypes.PackageTest
         public void HasPublishedMagicMgc()
         {
             var path = Path.Combine(
-                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), 
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
                 "publish", "magic.mgc"
             );
             Assert.True(File.Exists(path), PublishedFolderMissing);
         }
-        
+
         [Fact]
         public void HasPublishedLibMagicLinuxRuntime()
         {
             var path = Path.Combine(
-                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), 
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
                 "publish", "runtimes", "linux-x64", "native", "libmagic-1.so"
             );
             Assert.True(File.Exists(path), PublishedFolderMissing);
         }
-        
+
         [Fact]
         public void HasPublishedLibMagicMacOsRuntime()
         {
             var path = Path.Combine(
-                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), 
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
                 "publish", "runtimes", "osx-x64", "native", "libmagic-1.dylib"
             );
             Assert.True(File.Exists(path), PublishedFolderMissing);
         }
-        
+
         [Fact]
         public void HasPublishedLibMagicWindowsRuntime()
         {
             var path = Path.Combine(
-                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), 
-                "publish", "runtimes", "win-x64", "native", "libmagic-1.so"
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "publish", "runtimes", "win-x64", "native", "libmagic-1.dll"
             );
             Assert.True(File.Exists(path), PublishedFolderMissing);
+
+            var path2 = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "publish", "runtimes", "win-x64", "native", "libgnurx-0.dll"
+            );
+            Assert.True(File.Exists(path2), PublishedFolderMissing);
         }
     }
 }
